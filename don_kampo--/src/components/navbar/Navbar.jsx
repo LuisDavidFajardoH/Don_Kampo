247c7d9dc5889f475b2bc9c8223f78e42b987a60
DvidFH <luisda038@outlook.com>
2024-11-12 21:20:00 -0500

    Refactor Navbar layout for improved responsiveness and update cart icon display; change background color in Home page

--- don_kampo--/src/components/navbar/Navbar.jsx
+++ don_kampo--/src/components/navbar/Navbar.jsx
@@ -78,203 +78,188 @@ const Navbar = () => {
 
   return (
     <Header className="navbar">
-      <div className="logo" onClick={() => handleMenuClick("home", "/")}>
-        <img
-          src="/images/1.png"
-          alt="Don Kampo Logo"
-          style={{ height: "88px" }}
-        />
-      </div>
-
-      {/* Menú para pantallas grandes */}
-      <div className="menu-desktop">
-        <Menu
-          theme="light"
-          mode="horizontal"
-          selectedKeys={[selectedKey]}
-          className="menu"
-        >
-          <Menu.Item
-            key="home"
-            icon={<HomeOutlined />}
-            onClick={() => handleMenuClick("home", "/")}
-          >
-            Inicio
-          </Menu.Item>
-          <Menu.Item
-            key="products"
-            icon={<AppstoreOutlined />}
-            onClick={() => handleMenuClick("products", "/products")}
-          >
-            Productos
-          </Menu.Item>
-
-          <Menu.Item
-            key="cart"
-            onClick={() => handleMenuClick("cart", "/cart")}
-          >
-            <Badge
-              count={
-                cartValue > 99999
-                  ? `${(cartValue / 1000).toFixed(1)}K`
-                  : `$${cartValue.toLocaleString()}`
-              }
-              offset={[30, -10]} // Mueve el contador a la izquierda
-              style={{
-                backgroundColor: "#52c41a",
-                fontSize: "14px",
-                minWidth: "60px", // Aumenta el ancho mínimo
-                padding: "0 8px",
-              }}
-            >
-              <ShoppingCartOutlined
-                style={{ fontSize: "18px", color: "white" }}
-              />
-            </Badge>
-          </Menu.Item>
-
-          {isLoggedIn ? (
-            <>
-              {isAdmin && (
-                <Menu.Item
-                  key="createproduct"
-                  icon={<PlusOutlined />}
-                  onClick={() =>
-                    handleMenuClick("createproduct", "/createproduct")
-                  }
-                >
-                  Agregar Productos
-                </Menu.Item>
-              )}
-              <Menu.Item
-                key="profile"
-                icon={<UserOutlined />}
-                onClick={() => handleMenuClick("profile", "/profile")}
-              >
-                {loginData.user.user_name}
-              </Menu.Item>
-              <Menu.Item
-                key="logout"
-                icon={<LogoutOutlined />}
-                onClick={handleLogout}
-              >
-                Cerrar Sesión
-              </Menu.Item>
-            </>
-          ) : (
-            <>
-              <Menu.Item
-                key="login"
-                onClick={() => handleMenuClick("login", "/login")}
-              >
-                Iniciar Sesión
-              </Menu.Item>
-              <Menu.Item
-                key="register"
-                onClick={() => handleMenuClick("register", "/register")}
-              >
-                Registrarse
-              </Menu.Item>
-            </>
-          )}
-        </Menu>
-      </div>
-
-      {/* Ícono de menú para pantallas pequeñas */}
-      <Button
-        className="menu-mobile-button"
-        type="text"
-        icon={<MenuOutlined />}
-        onClick={() => setDrawerVisible(true)}
+  <div className="navbar-left">
+    {/* Logo */}
+    <div className="logo" onClick={() => handleMenuClick("home", "/")}>
+      <img
+        src="/images/1.png"
+        alt="Don Kampo Logo"
+        style={{ height: "88px" }}
       />
+    </div>
 
-      {/* Drawer para el menú colapsable en pantallas pequeñas */}
-      <Drawer
-        title="Menú"
-        placement="right"
-        closable
-        onClose={() => setDrawerVisible(false)}
-        visible={drawerVisible}
+    {/* Carrito */}
+    <div className="cart-icon" onClick={() => handleMenuClick("cart", "/cart")}>
+      <Badge
+        count={
+          cartValue > 99999
+            ? `${(cartValue / 1000).toFixed(1)}K`
+            : `$${cartValue.toLocaleString()}`
+        }
+        offset={[10, 0]}
+        style={{
+          backgroundColor: "#52c41a",
+          fontSize: "14px",
+          padding: "0 8px",
+        }}
       >
-        <Menu theme="light" mode="vertical" selectedKeys={[selectedKey]}>
-          <Menu.Item
-            key="home"
-            icon={<HomeOutlined />}
-            onClick={() => handleMenuClick("home", "/")}
-          >
-            Inicio
-          </Menu.Item>
-          <Menu.Item
-            key="products"
-            icon={<AppstoreOutlined />}
-            onClick={() => handleMenuClick("products", "/products")}
-          >
-            Productos
-          </Menu.Item>
+        <ShoppingCartOutlined
+          style={{ fontSize: "24px", color: "white", cursor: "pointer" }}
+        />
+      </Badge>
+    </div>
+  </div>
 
-          <Menu.Item key="cart" onClick={() => navigate("/cart")}>
-            <Badge
-              count={
-                cartValue > 99999
-                  ? `${(cartValue / 1000).toFixed(1)}K`
-                  : `$${cartValue.toLocaleString()}`
+  {/* Menú para pantallas grandes */}
+  <div className="menu-desktop">
+    <Menu
+      theme="light"
+      mode="horizontal"
+      selectedKeys={[selectedKey]}
+      className="menu"
+    >
+      <Menu.Item
+        key="home"
+        icon={<HomeOutlined />}
+        onClick={() => handleMenuClick("home", "/")}
+      >
+        Inicio
+      </Menu.Item>
+      <Menu.Item
+        key="products"
+        icon={<AppstoreOutlined />}
+        onClick={() => handleMenuClick("products", "/products")}
+      >
+        Productos
+      </Menu.Item>
+
+      {isLoggedIn ? (
+        <>
+          {isAdmin && (
+            <Menu.Item
+              key="createproduct"
+              icon={<PlusOutlined />}
+              onClick={() =>
+                handleMenuClick("createproduct", "/createproduct")
               }
-              offset={[10, 0]}
-              style={{ backgroundColor: "#52c41a", fontSize: "14px" }}
             >
-              <ShoppingCartOutlined
-                style={{ fontSize: "18px", color: "white" }}
-              />
-            </Badge>
-          </Menu.Item>
-
-          {isLoggedIn ? (
-            <>
-              {isAdmin && (
-                <Menu.Item
-                  key="createproduct"
-                  icon={<PlusOutlined />}
-                  onClick={() =>
-                    handleMenuClick("createproduct", "/createproduct")
-                  }
-                >
-                  Agregar Productos
-                </Menu.Item>
-              )}
-              <Menu.Item
-                key="profile"
-                icon={<UserOutlined />}
-                onClick={() => handleMenuClick("profile", "/profile")}
-              >
-                {loginData.user.user_name}
-              </Menu.Item>
-              <Menu.Item
-                key="logout"
-                icon={<LogoutOutlined />}
-                onClick={handleLogout}
-              >
-                Cerrar Sesión
-              </Menu.Item>
-            </>
-          ) : (
-            <>
-              <Menu.Item
-                key="login"
-                onClick={() => handleMenuClick("login", "/login")}
-              >
-                Iniciar Sesión
-              </Menu.Item>
-              <Menu.Item
-                key="register"
-                onClick={() => handleMenuClick("register", "/register")}
-              >
-                Registrarse
-              </Menu.Item>
-            </>
+              Agregar Productos
+            </Menu.Item>
           )}
-        </Menu>
-      </Drawer>
-    </Header>
+          <Menu.Item
+            key="profile"
+            icon={<UserOutlined />}
+            onClick={() => handleMenuClick("profile", "/profile")}
+          >
+            {loginData.user.user_name}
+          </Menu.Item>
+          <Menu.Item
+            key="logout"
+            icon={<LogoutOutlined />}
+            onClick={handleLogout}
+          >
+            Cerrar Sesión
+          </Menu.Item>
+        </>
+      ) : (
+        <>
+          <Menu.Item
+            key="login"
+            onClick={() => handleMenuClick("login", "/login")}
+          >
+            Iniciar Sesión
+          </Menu.Item>
+          <Menu.Item
+            key="register"
+            onClick={() => handleMenuClick("register", "/register")}
+          >
+            Registrarse
+          </Menu.Item>
+        </>
+      )}
+    </Menu>
+  </div>
+
+  {/* Ícono de menú para pantallas pequeñas */}
+  <Button
+    className="menu-mobile-button"
+    type="text"
+    icon={<MenuOutlined />}
+    onClick={() => setDrawerVisible(true)}
+  />
+
+  {/* Drawer para menú en pantallas pequeñas */}
+  <Drawer
+    title="Menú"
+    placement="right"
+    closable
+    onClose={() => setDrawerVisible(false)}
+    visible={drawerVisible}
+  >
+    <Menu theme="light" mode="vertical" selectedKeys={[selectedKey]}>
+      <Menu.Item
+        key="home"
+        icon={<HomeOutlined />}
+        onClick={() => handleMenuClick("home", "/")}
+      >
+        Inicio
+      </Menu.Item>
+      <Menu.Item
+        key="products"
+        icon={<AppstoreOutlined />}
+        onClick={() => handleMenuClick("products", "/products")}
+      >
+        Productos
+      </Menu.Item>
+
+      {isLoggedIn ? (
+        <>
+          {isAdmin && (
+            <Menu.Item
+              key="createproduct"
+              icon={<PlusOutlined />}
+              onClick={() =>
+                handleMenuClick("createproduct", "/createproduct")
+              }
+            >
+              Agregar Productos
+            </Menu.Item>
+          )}
+          <Menu.Item
+            key="profile"
+            icon={<UserOutlined />}
+            onClick={() => handleMenuClick("profile", "/profile")}
+          >
+            {loginData.user.user_name}
+          </Menu.Item>
+          <Menu.Item
+            key="logout"
+            icon={<LogoutOutlined />}
+            onClick={handleLogout}
+          >
+            Cerrar Sesión
+          </Menu.Item>
+        </>
+      ) : (
+        <>
+          <Menu.Item
+            key="login"
+            onClick={() => handleMenuClick("login", "/login")}
+          >
+            Iniciar Sesión
+          </Menu.Item>
+          <Menu.Item
+            key="register"
+            onClick={() => handleMenuClick("register", "/register")}
+          >
+            Registrarse
+          </Menu.Item>
+        </>
+      )}
+    </Menu>
+  </Drawer>
+</Header>
+
   );
 };
 
