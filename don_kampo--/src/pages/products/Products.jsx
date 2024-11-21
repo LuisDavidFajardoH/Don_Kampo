9716eb669024f0fecbf076dea01dd01c29eac2c9
DvidFH <luisda038@outlook.com>
2024-11-21 06:57:24 -0500

    Refactor: implement pagination for product listing, enhancing user experience with improved navigation and layout adjustments

--- don_kampo--/src/pages/products/Products.jsx
+++ don_kampo--/src/pages/products/Products.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import Navbar from "../../components/navbar/Navbar";
 import CustomFooter from "../../components/footer/Footer";
 import BotonWhatsapp from "../../components/botonWhatsapp/BotonWhatsapp";
-import { Card, Button, message, Select, Input } from "antd";
+import { Card, Button, message, Select, Input, Pagination } from "antd";
 import axios from "axios";
 import { useCart } from "../../pages/products/CartContext";
 import "./Products.css";
@@ -10,6 +10,7 @@ import "./Products.css";
 const { Option } = Select;
 
 const Products = () => {
+  // Estados para manejar productos y filtros
   const [products, setProducts] = useState([]);
   const [filteredProducts, setFilteredProducts] = useState([]);
   const [loading, setLoading] = useState(true);
@@ -17,6 +18,18 @@ const Products = () => {
   const [selectedCategory, setSelectedCategory] = useState("Todas");
   const [searchQuery, setSearchQuery] = useState("");
   const [selectedVariations, setSelectedVariations] = useState({});
+
+  // Estados para la paginación
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemsPerPage] = useState(10); // Número de productos por página
+
+  // Cálculo de los índices para paginar
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentProducts = filteredProducts.slice(
+    indexOfFirstItem,
+    indexOfLastItem
+  );
 
   const { cart, addToCart, removeFromCart } = useCart();
 
@@ -48,7 +61,7 @@ const Products = () => {
         ];
         setCategories(uniqueCategories);
 
-        const params = new URLSearchParams(location.search);
+        const params = new URLSearchParams(window.location.search);
         const initialCategory = params.get("category");
         const initialSearch = params.get("search");
 
@@ -82,10 +95,14 @@ const Products = () => {
   };
 
   const normalizeString = (str) => {
-    return str.normalize("NFD").replace(/[̀-ͯ]/g, "").toLowerCase();
+    return str.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLowerCase();
   };
 
-  const filterProducts = (category, query, productsToFilter = products) => {
+  const filterProducts = (
+    category,
+    query,
+    productsToFilter = products
+  ) => {
     const filtered = productsToFilter.filter((product) => {
       const matchesCategory =
         category === "Todas" || product.category === category;
@@ -95,6 +112,7 @@ const Products = () => {
       return matchesCategory && matchesSearch;
     });
     setFilteredProducts(filtered);
+    setCurrentPage(1); // Reiniciar a la página 1 cuando se filtra
   };
 
   useEffect(() => {
@@ -105,8 +123,6 @@ const Products = () => {
     // Si no hay URL, usar una imagen predeterminada desde public/images
     return photoUrl || `${process.env.PUBLIC_URL}/images/icon.png`;
   };
-  
-  
 
   const getPriceByUserType = (product) => {
     switch (userType) {
@@ -197,81 +213,102 @@ const Products = () => {
         {loading ? (
           <p>Cargando productos...</p>
         ) : (
-          filteredProducts.map((product) => (
-            <Card
-              key={product.product_id}
-              className="product-card"
-              hoverable
-              cover={
-                <img
-                  alt={product.name}
-                  src={getBase64Image(product.photo_url)} // Cambiado para usar photo_url
-                  style={{ objectFit: "cover", width: "100%", height: "200px" }}
-                />
-              }
-            >
-              <div className="product-info">
-                <p className="product-category">{product.category}</p>
-                <h3 className="product-name">{product.name}</h3>
+          <>
+            {/* Mapeo de productos paginados */}
+            {currentProducts.map((product) => (
+              <Card
+                key={product.product_id}
+                className="product-card"
+                hoverable
+                cover={
+                  <img
+                    alt={product.name}
+                    src={getBase64Image(product.photo_url)}
+                    style={{
+                      objectFit: "cover",
+                      width: "100%",
+                      height: "200px",
+                    }}
+                  />
+                }
+              >
+                <div className="product-info">
+                  <p className="product-category">{product.category}</p>
+                  <h3 className="product-name">{product.name}</h3>
 
-                <Select
-                  placeholder="Selecciona una variación"
-                  style={{ width: "100%", marginBottom: 8 }}
-                  onChange={(value) =>
-                    handleVariationChange(product.product_id, value)
-                  }
-                  value={selectedVariations[product.product_id] || undefined}
-                >
-                  {product.variations.map((variation) => (
-                    <Option
-                      key={variation.variation_id}
-                      value={variation.variation_id}
-                    >
-                      {`${variation.quality} - ${variation.quantity}`}
-                    </Option>
-                  ))}
-                </Select>
-
-                <p className="product-price">
-                  {getSelectedVariationPrice(product) !== null
-                    ? `$${parseFloat(
-                        getSelectedVariationPrice(product)
-                      ).toLocaleString()}`
-                    : "Selecciona una variación para ver el precio."}
-                </p>
-
-                {cart[product.product_id] ? (
-                  <div className="quantity-controls">
-                    <Button
-                      onClick={() => handleRemoveFromCart(product)}
-                      className="quantity-button"
-                    >
-                      -
-                    </Button>
-                    <span className="quantity-text">
-                      {cart[product.product_id].quantity}
-                    </span>
-                    <Button
-                      onClick={() => handleAddToCart(product)}
-                      className="quantity-button"
-                    >
-                      +
-                    </Button>
-                  </div>
-                ) : (
-                  <Button
-                    type="primary"
-                    onClick={() => handleAddToCart(product)}
-                    className="add-to-cart-button"
+                  <Select
+                    placeholder="Selecciona una variación"
+                    style={{ width: "100%", marginBottom: 8 }}
+                    onChange={(value) =>
+                      handleVariationChange(product.product_id, value)
+                    }
+                    value={
+                      selectedVariations[product.product_id] || undefined
+                    }
                   >
-                    Añadir al carrito
-                  </Button>
-                )}
-              </div>
-            </Card>
-          ))
+                    {product.variations.map((variation) => (
+                      <Option
+                        key={variation.variation_id}
+                        value={variation.variation_id}
+                      >
+                        {`${variation.quality} - ${variation.quantity}`}
+                      </Option>
+                    ))}
+                  </Select>
+
+                  <p className="product-price">
+                    {getSelectedVariationPrice(product) !== null
+                      ? `$${parseFloat(
+                          getSelectedVariationPrice(product)
+                        ).toLocaleString()}`
+                      : "Selecciona una variación para ver el precio."}
+                  </p>
+
+                  {cart[product.product_id] ? (
+                    <div className="quantity-controls">
+                      <Button
+                        onClick={() => handleRemoveFromCart(product)}
+                        className="quantity-button"
+                      >
+                        -
+                      </Button>
+                      <span className="quantity-text">
+                        {cart[product.product_id].quantity}
+                      </span>
+                      <Button
+                        onClick={() => handleAddToCart(product)}
+                        className="quantity-button"
+                      >
+                        +
+                      </Button>
+                    </div>
+                  ) : (
+                    <Button
+                      type="primary"
+                      onClick={() => handleAddToCart(product)}
+                      className="add-to-cart-button"
+                    >
+                      Añadir al carrito
+                    </Button>
+                  )}
+                </div>
+              </Card>
+            ))}
+          </>
         )}
       </div>
+
+      {/* Componente de paginación */}
+      {!loading && (
+        <Pagination
+          current={currentPage}
+          pageSize={itemsPerPage}
+          total={filteredProducts.length}
+          onChange={(page) => setCurrentPage(page)}
+          className="pagination"
+        />
+      )}
+
       <BotonWhatsapp />
       <CustomFooter />
     </>
